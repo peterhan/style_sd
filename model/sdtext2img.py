c22846d8c9a91e4d65ff8e616178a3ecd52b345a
hanshu <hanshu@autohome.com.cn>
2023-05-23 10:25:30 +0800

    add features

---
+++ model/sdtext2img.py
@@ -0,0 +1,62 @@
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+from tornado import gen
+from tornado.options import define, options
+import logging
+
+import requests
+import json
+import time
+import pdb
+
+if __name__ == '__main__':
+    import sys
+    sys.path.append('..')
+    
+from _key_store import SD_API_KEY
+
+class ModelSDText2IMG():
+        
+    def prompt2img(self,prompt,para={}):
+        url = "https://stablediffusionapi.com/api/v3/text2img"
+        # prompt="handsome chinese male dressed in sport wear sitting in dinning room"
+        confd={
+          "key": SD_API_KEY,
+          "prompt": prompt,
+          "negative_prompt": None,
+          "width": "512",
+          "height": "512",
+          "samples": "1",
+          "num_inference_steps": "20",
+          "seed": None,
+          "guidance_scale": 7.5,
+          "safety_checker": "yes",
+          "multi_lingual": "no",
+          "panorama": "no",
+          "self_attention": "no",
+          "upscale": "no",
+          "embeddings_model": "embeddings_model_id",
+          "webhook": None,
+          "track_id": None
+        }
+        confd.update(para)
+        payload = json.dumps(confd)
+        
+        headers = {
+          'Content-Type': 'application/json'
+        }
+        resp = requests.request("POST", url, headers=headers, data=payload)
+
+        jo = resp.json()
+        js = json.dumps(jo,indent=2)
+        logging.debug(js)
+        ret_url = jo['output'][0]
+        ret_eta = 0
+        if 'eta' in jo:
+            ret_eta = math.ceil(float(jo['eta']))       
+        return ret_eta,ret_url,jo
+        
+if __name__ == '__main__':
+    sdt  = SDText2IMG(None,None)
---
+++ run.py
@@ -0,0 +1,56 @@
+#!coding:utf8
+from datetime import datetime,timedelta
+import time
+from multiprocessing.pool import ThreadPool
+import logging
+import traceback
+import json
+import sys
+import requests
+
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+from tornado import gen
+from tornado.options import define, options
+
+from model.sdtext2img import ModelSDText2IMG
+
+
+if 'prod' in sys.argv:
+    print('Production Mode')    
+    define("port", default=8899, help="run on the given port", type=int)
+else:
+    print('Test Mode')    
+    define("port", default=8898, help="run on the given port", type=int)
+
+
+class IndexHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write('style api')
+
+class SDText2IMG(tornado.web.RequestHandler):
+    def get(self):
+        msdt = ModelSDText2IMG()
+        prompt = self.get_argument('prompt').replace(',',' ')
+        ret_eta,ret_url,jo = msdt.prompt2img(prompt)
+        ret_d = {'status':'ok','data':{'eta':ret_eta,'url':ret_url}}
+        self.write(json.dumps(ret_d))
+    
+    def post(self,action):
+        jo = tornado.escape.json_decode(self.request.body)
+        self.write(''+action)  
+    
+if __name__ == "__main__":
+    tornado.options.parse_command_line()
+    app = tornado.web.Application(
+        handlers=[
+            (r"/", IndexHandler),
+            (r"/style_sd/text2img", SDText2IMG),
+        ]
+        ,debug=True
+    )
+    http_server = tornado.httpserver.HTTPServer(app)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
