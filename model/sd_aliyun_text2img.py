bc3f088e162fd28051cc60d4879e0c76cfd99f66
hanshu <hanshu@autohome.com.cn>
2023-05-24 17:54:54 +0800

    add features

--- model/sd_aliyun_text2img.py
+++ model/sd_aliyun_text2img.py
@@ -1,3 +1,4 @@
+#!coding:utf8
 import tornado.httpserver
 import tornado.ioloop
 import tornado.options
@@ -23,12 +24,43 @@ from PIL import Image, PngImagePlugin
 from _key_store import AL_URL,AL_TOKEN
 
 class ModelSDText2IMG(object):
+    '''/sdapi/v1/txt2img 文字生图 POST
+    /sdapi/v1/img2img 图片生图 POST
+    /sdapi/v1/options 获取设置 GET | 更新设置 POST（可用来更新远端的模型）
+    /sdapi/v1/sd-models 获取所有的模型 GET
+    '''
     def prompt2img(self,prompt,para={}):
         url = AL_URL        
         # prompt="handsome chinese male dressed in sport wear sitting in dinning room"
-        confd= {"prompt": prompt, "all_prompts": [prompt], "negative_prompt": "", "all_negative_prompts": [""], "seed": 2852570473, "all_seeds": [2852570473], "subseed": 3611650335, "all_subseeds": [3611650335], "subseed_strength": 0, "width": 512, "height": 512, "sampler_name": "Euler a", "cfg_scale": 7, "steps": 10, "batch_size": 1, "restore_faces": False, "face_restoration_model": None, "sd_model_hash": "6ce0161689", "seed_resize_from_w": 0, "seed_resize_from_h": 0, "denoising_strength": None, "extra_generation_params": {}, "index_of_first_image": 0, "styles": [], "job_timestamp": "20230524061323", "clip_skip": 1, "is_using_inpainting_conditioning": False}
+        confd= {
+         "denoising_strength": 0,
+         "prompt": "puppy dogs", #提示词
+         "negative_prompt": "", #反向提示词
+         "seed": -1, #种子，随机数
+         "batch_size": 2, #每次张数
+         "n_iter": 1, #生成批次
+         "steps": 50, #生成步数
+         "cfg_scale": 7, #关键词相关性
+         "width": 512, #宽度
+         "height": 512, #高度
+         "restore_faces": False, #脸部修复
+         "tiling": False, #可平埔
+         "override_settings": {
+             "sd_model_checkpoint" :"wlop-any.ckpt [7331f3bc87]"
+          }, # 一般用于修改本次的生成图片的stable diffusion 模型，用法需保持一致
+           "script_args": [
+              0,
+              True,
+              True,
+              "LoRA",
+              "dingzhenlora_v1(fa7c1732cc95)",
+              1,
+              1
+          ], # 一般用于lora模型或其他插件参数，如示例，我放入了一个lora模型， 1，1为两个权重值，一般只用到前面的权重值1
+         "sampler_index": "Euler" #采样方法
+        }
         confd.update(para)
-        payload = json.dumps(confd)
+        payload = confd
         
         token = AL_TOKEN
         encoded_token = base64.b64encode(token.encode("utf-8")).decode("utf-8")
